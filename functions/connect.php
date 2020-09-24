9b4562d5288e41588df33f0204ce2f37c7df3e6d
xraybrain <xraybrain@gmail.com>
2020-09-25 00:24:08 +0100

    Configured Database

--- functions/connect.php
+++ functions/connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "db_elearning";
+$host = "us-cdbr-east-02.cleardb.com";
+$user = "b37a8abc52df42";
+$password = "017b786c";
+$db = "heroku_008a42196f61275	";
 
 $con = mysqli_connect($host,$user,$password) or die("Could not connect to database");
 mysqli_select_db($con, $db) or die("No database selected");
--- lecturer/topic/function.php
+++ lecturer/topic/function.php
@@ -2,14 +2,14 @@
 
 
 function dbcon(){
-	$host = "localhost";
-	$user = "root";
-	$pwd = "";
-	$db = "db_elearning";
-
+	$host = "us-cdbr-east-02.cleardb.com";
+	$user = "b37a8abc52df42";
+	$password = "017b786c";
+	$db = "heroku_008a42196f61275	";
+	
 	global $con;
 	$con = mysqli_connect($host,$user,$pwd) or die ("ERROR Connecting to Database");
-
+	
 	$sel = mysqli_select_db($con,$db) or die(mysqli_error($con));
 }
 
@@ -18,28 +18,28 @@ function dbclose(){
 	$user = "root";
 	$pwd = "";
 	$db = "db_elearning";
-
+	
 	$con = mysqli_connect($host,$user,$pwd) or die ("ERROR Connecting to Database");
-
+	
 	$sel = mysqli_select_db($con,$db);
-
+	
 	mysqli_close($con);
 }
 
 function category(){
 	dbcon();
 	global $con;
-
+	
 	$sel = mysqli_query($con,"SELECT * from tbl_category");
-
+	
 	if($sel==true){
 		while($row=mysqli_fetch_assoc($sel)){
 			extract($row);
 			echo '<option value='.$cat_Id.'>'.$name.'</option>';
 		}
 	}
-
-
+	
+	
 	dbclose();
 }
 function sub(){
@@ -47,15 +47,15 @@ function sub(){
 	global $con;
 	
 	$sel = mysqli_query($con,"SELECT * from tbl_topic");
-
+	
 	if($sel==true){
 		while($row=mysqli_fetch_assoc($sel)){
 			extract($row);
 			echo '<option value='.$topic_Id.'>'.$title.'</option>';
 		}
 	}
-
-
+	
+	
 	dbclose();
 }
 
