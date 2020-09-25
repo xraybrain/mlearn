7c8bff832ef9f77fa6d2fded21cd0279e6469a83
xraybrain <xraybrain@gmail.com>
2020-09-25 06:57:51 +0100

    Configured Database - 2

--- functions/connect.php
+++ functions/connect.php
@@ -2,7 +2,7 @@
 $host = "us-cdbr-east-02.cleardb.com";
 $user = "b37a8abc52df42";
 $password = "017b786c";
-$db = "heroku_008a42196f61275	";
+$db = "heroku_008a42196f61275";
 
 $con = mysqli_connect($host,$user,$password) or die("Could not connect to database");
 mysqli_select_db($con, $db) or die("No database selected");
